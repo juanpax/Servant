7aa3fb1cc38f1d3a85e6ff9efcad009d53f3abb9
jujimen <juanpajc08@gmail.com>
2019-08-28 20:15:09 -0600

    Now it is parsing the pressed key to the right string, it does not matter if it is a special character it will be get parsed as it should

--- Servant/Servant/Program.cs
+++ Servant/Servant/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Servant
@@ -14,9 +15,9 @@ namespace Servant
         private static IntPtr _hookID = IntPtr.Zero;
 
         // Testing variables
-        static string pattern = "TEST";
+        static string pattern = "..TEST ";
         static string currentString = "";
-        static string template = "THIS is a ** ? test TEXT";
+        static string template = "THIS is A TEXT";
 
         [STAThread]
         static void Main()
@@ -41,23 +42,24 @@ namespace Servant
 
         private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
+        //lParam => Unmanaged memory 
         private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
+            // lParam could be a memory position 
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
-                int key = Marshal.ReadInt32(lParam);
-                currentString += ((Keys)key).ToString();
+                currentString += VKCodeToString((uint)Marshal.ReadInt32(lParam)); //https://stackoverflow.com/questions/10391025/receive-os-level-key-press-events-in-c-sharp-application
+                Console.WriteLine(currentString);
 
                 if (pattern == currentString)
                 {
                     UnhookWindowsHookEx(_hookID);
                     //Missing functionality: Find the template based on the match
 
-                    keybd_event((byte)key, 0, 0, IntPtr.Zero);
-
-                    foreach (int letter in template)
+                    foreach (char letter in template)
                     {
-                        keybd_event((byte)letter, 0, 0, IntPtr.Zero);
+                        Console.WriteLine(letter);
+                        keybd_event((byte)letter, 0, 0, IntPtr.Zero); //https://stackoverflow.com/questions/31370634/how-to-sendkeybd-event-unicode-keys-with-c-sharp
                     }
 
                     currentString = "";
@@ -67,11 +69,152 @@ namespace Servant
                 {
                     currentString = "";
                 }
-
             }
 
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
+
+        //...................................Testing..........................................
+
+        private static uint _lastVKCode = 0;
+        private static uint _lastScanCode = 0;
+        private static byte[] _lastKeyState = new byte[255];
+        private static bool _lastIsDead = false;
+
+        public static string VKCodeToString(uint vkCode)
+        {
+            // ToUnicodeEx needs StringBuilder, it populates that during execution.
+            var sbString = new System.Text.StringBuilder(5);
+
+            var bKeyState = new byte[255];
+            bool bKeyStateStatus;
+            bool isDead = false;
+
+            // Gets the current windows window handle, threadID, processID
+            IntPtr currentHWnd = GetForegroundWindow();
+            uint currentProcessID;
+            uint currentWindowThreadID = GetWindowThreadProcessId(currentHWnd, out currentProcessID);
+
+            // This programs Thread ID
+            uint thisProgramThreadId = GetCurrentThreadId();
+
+            // Attach to active thread so we can get that keyboard state
+            if (AttachThreadInput(thisProgramThreadId, currentWindowThreadID, true))
+            {
+                // Current state of the modifiers in keyboard
+                bKeyStateStatus = GetKeyboardState(bKeyState);
+
+                // Detach
+                AttachThreadInput(thisProgramThreadId, currentWindowThreadID, false);
+            }
+            else
+            {
+                // Could not attach, perhaps it is this process?
+                bKeyStateStatus = GetKeyboardState(bKeyState);
+            }
+
+            // On failure we return empty string.
+            if (!bKeyStateStatus)
+                return "";
+
+            // Gets the layout of keyboard
+            IntPtr hkl = GetKeyboardLayout(currentWindowThreadID);
+
+            // Maps the virtual keycode
+            uint lScanCode = MapVirtualKeyEx(vkCode, 0, hkl);
+
+            
+
+            // Converts the VKCode to unicode
+            int relevantKeyCountInBuffer = ToUnicodeEx(vkCode, lScanCode, bKeyState, sbString, sbString.Capacity, (uint)0, hkl);
+
+            string ret = "";
+
+            switch (relevantKeyCountInBuffer)
+            {
+                // Dead keys (^,`...)
+                case -1:
+                    isDead = true;
+
+                    // We must clear the buffer because ToUnicodeEx messed it up, see below.
+                    ClearKeyboardBuffer(vkCode, lScanCode, hkl);
+                    break;
+
+                case 0:
+                    break;
+
+                // Single character in buffer
+                case 1:
+                    ret = sbString[0].ToString();
+                    break;
+
+                // Two or more (only two of them is relevant)
+                case 2:
+                default:
+                    ret = sbString.ToString().Substring(0, 2);
+                    break;
+            }
+
+            if (_lastVKCode != 0 && _lastIsDead)
+            {
+                var sbTemp = new System.Text.StringBuilder(5);
+                ToUnicodeEx(_lastVKCode, _lastScanCode, _lastKeyState, sbTemp, sbTemp.Capacity, (uint)0, hkl);
+                _lastVKCode = 0;
+
+                return ret;
+            }
+
+            // Save these
+            _lastScanCode = lScanCode;
+            _lastVKCode = vkCode;
+            _lastIsDead = isDead;
+            _lastKeyState = (byte[])bKeyState.Clone();
+
+            return ret;
+        }
+
+        private static void ClearKeyboardBuffer(uint vk, uint sc, IntPtr hkl)
+        {
+            var sb = new System.Text.StringBuilder(10);
+
+            int rc;
+            do
+            {
+                var lpKeyStateNull = new Byte[255];
+                rc = ToUnicodeEx(vk, sc, lpKeyStateNull, sb, sb.Capacity, 0, hkl);
+            } while (rc < 0);
+        }
+
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("User32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint GetCurrentThreadId();
+
+        [DllImport("user32.dll")]
+        private static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetKeyboardLayout(uint dwLayout);
+
+        [DllImport("user32.dll")]
+        private static extern uint MapVirtualKeyEx(uint uCode, uint uMapType, IntPtr dwhkl);
+
+        [DllImport("user32.dll")]
+        private static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
+
+
+        //..............
+
+
+
 
         // Get OS key events to get the key clicked, this is basically like a keylogger 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
@@ -91,14 +234,28 @@ namespace Servant
         public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
 
     }
+
 }
+
 
 /*
  * Bugs:
  * 
- * Si el template contiene caracteres especiales no los esta pegando
- * No esta validando si el patron contiene caracteres especiales 
+ * Si el template contiene caracteres especiales no los esta pegando 
+ * No esta validando si el patron contiene caracteres especiales => ahora si funciona con caracteres especiales, pero no caracteres especiales de combinaciones de teclas
  * Si el patron contiene minusculas 
  * Si el template tiene minusculas
  * 
+ * Hay un problema con el parse de numeros a chars, no siempre esta viniendo el numero que corresponde a la letra o simbolo especial
+ * 
+ *  http://yorktown.cbe.wwu.edu/sandvig/shared/asciiCodes.aspx
+ *  https://docs.microsoft.com/en-us/dotnet/api/system.intptr?view=netframework-4.8 => No se que esta haciendo
+ *  https://stackoverflow.com/questions/11829072/how-do-i-read-a-uint-from-a-pointer-with-marshalling
+ *  
+ *  https://stackoverflow.com/questions/20201350/paste-text-to-an-active-word-window-using-c-sharp
+ *  Parece que esta es la solucion: 
+ *  https://www.youtube.com/watch?v=f_ZBFJMlhYQ
+ *  
+ *  Esto fue lo que sirvio para parsear bien la tecla al valor 
+ *  
  */
